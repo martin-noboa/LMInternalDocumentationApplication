fde9e0308aaf69c311b1dcb725c4fcc1c88de73a
martin-noboa <martin.noboa@nibbio.co>
2024-04-29 22:56:44 -0600

    Update basic react files

---
+++ frontend_service/src/components/Markdown.jsx
@@ -0,0 +1,14 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+
+function Markdown() {
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <p>Content</p>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default Markdown;
---
+++ frontend_service/src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#10121e', 
+    },
+    secondary: {
+      main: '#dc004e', 
+    },
+  },
+});
+
+export default theme;
