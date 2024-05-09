9a7fe0775fd2bac39c2334d62c715da07273a29e
martin-noboa <martin.noboa@nibbio.co>
2024-05-08 20:31:06 -0600

    feat: update app router switch PENDING

--- frontend_service/src/App.jsx
+++ frontend_service/src/App.jsx
@@ -1,21 +1,22 @@
 import Navbar from './components/Navbar';
 import Markdown from './components/Markdown';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 
 function App() {
     return (
     <div>
         <Navbar />
-        <Router>
-        <Switch>
-            <Route path="">
-                <Markdown filename="index.md" />
-            </Route>
-            <Route path="/test">
-                <Markdown filename="test.md" />
-            </Route>
-        </Switch>
-        </Router>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/">
+                    <Markdown filename="index.md" />
+                </Route>
+                <Route path="/test">
+                    <Markdown filename="test.md" />
+                </Route>
+            </Switch>
+        </BrowserRouter>
     </div>
     );
 } 
